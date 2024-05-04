9a86123295024bf677e59a04794a01a59a981418
samuelM22 <admin@admins-MacBook-Pro.local>
2024-05-04 10:26:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Eden Cloud</h1>
+      <h1>Welcome to Eden Cloud Technologies</h1>
       <p>At Eden, we are commited to bringing technological and innovative teachings to the underserved.
         Our greatest mission is to see a generation embrace the dynamic impact of cutting edge technology, utilizing 
         the power of knowledge for the betterment of humanity.
